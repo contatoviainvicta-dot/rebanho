0b4573e478f0f0d1ef8080c31d7e512330d04015
contatoviainvicta-dot <contato.viainvicta@gmail.com>
2026-04-13 16:52:39 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,28 @@
-import sqlite3
-
-def conectar():
-    return sqlite3.connect("data.db", check_same_thread=False)
-
 def criar_tabelas():
     conn = conectar()
     cursor = conn.cursor()
 
-    # tabela animais
+    # LOTES
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS lotes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        descricao TEXT,
+        data TEXT
+    )
+    """)
+
+    # ANIMAIS
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS animais (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         identificacao TEXT,
-        idade INTEGER
+        idade INTEGER,
+        lote_id INTEGER
     )
     """)
 
-    # tabela pesagens
+    # PESAGENS
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS pesagens (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,60 +34,3 @@ def criar_tabelas():
 
     conn.commit()
     conn.close()
-
-
-# -----------------------
-# ANIMAIS
-# -----------------------
-
-def adicionar_animal(identificacao, idade):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    INSERT INTO animais (identificacao, idade)
-    VALUES (?, ?)
-    """, (identificacao, idade))
-
-    conn.commit()
-    conn.close()
-
-def listar_animais():
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM animais")
-    dados = cursor.fetchall()
-
-    conn.close()
-    return dados
-
-
-# -----------------------
-# PESAGENS
-# -----------------------
-
-def adicionar_pesagem(animal_id, peso, data):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    INSERT INTO pesagens (animal_id, peso, data)
-    VALUES (?, ?, ?)
-    """, (animal_id, peso, data))
-
-    conn.commit()
-    conn.close()
-
-def listar_pesagens(animal_id):
-    conn = conectar()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    SELECT * FROM pesagens WHERE animal_id = ?
-    """, (animal_id,))
-
-    dados = cursor.fetchall()
-
-    conn.close()
-    return dados
