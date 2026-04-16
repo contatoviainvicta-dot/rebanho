8facb3c169f6ca0be672da630a9a26ce1b795322
contatoviainvicta-dot <contato.viainvicta@gmail.com>
2026-04-16 19:41:47 -0300

    Update database.py

--- database.py
+++ database.py
@@ -177,3 +177,9 @@ def salvar_ocorrencia_mem(animal_id, data, tipo, descricao, gravidade):
         "descricao": descricao,
         "gravidade": gravidade
     })
+
+def listar_ocorrencias_mem(animal_id):
+    return [
+        o for o in st.session_state.ocorrencias
+        if o["animal_id"] == animal_id
+    ]
