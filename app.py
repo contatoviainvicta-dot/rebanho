ac3e828c5619ddb0900e21884e7b99480bed043c
contatoviainvicta-dot <contato.viainvicta@gmail.com>
2026-05-03 22:15:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1520,7 +1520,6 @@ for lote in listar_lotes():
             f"🟢 {nome_lote}: Situação controlada (Incidência {incidencia:.1f}%, GMD {gmd_medio:.2f})"
         )
 # dashboard executivo
-
 elif menu == "Dashboard Executivo":
     st.title("📊 Dashboard Executivo")
 
